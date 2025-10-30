d469f89fe94b89068ee041a050fcf9606fe69bf7
Maikel Baumann <maikel.baumann.gmx.ch@zbw-online.ch>
2025-10-30 21:54:57 +0100

    Test CI/CD

--- src/test/java/ch/flossrennen/eventmanagementsystem/EventManagementSystemApplicationTests.java
+++ src/test/java/ch/flossrennen/eventmanagementsystem/EventManagementSystemApplicationTests.java
@@ -1,9 +1,13 @@
 package ch.flossrennen.eventmanagementsystem;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.NONE,
+        classes = {EventManagementSystemApplication.class, SecurityAutoConfiguration.class}
+)
 class EventManagementSystemApplicationTests {
 
     @Test
