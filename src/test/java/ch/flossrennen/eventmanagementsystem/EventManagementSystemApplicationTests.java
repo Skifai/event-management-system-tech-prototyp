f7402609d8e127c3e7373225fe7624598c4479ff
Maikel Baumann <maikel.baumann.gmx.ch@zbw-online.ch>
2025-11-01 17:59:41 +0100

    Change to Postgre Tests

--- src/test/java/ch/flossrennen/eventmanagementsystem/EventManagementSystemApplicationTests.java
+++ src/test/java/ch/flossrennen/eventmanagementsystem/EventManagementSystemApplicationTests.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
-@EnableAutoConfiguration(exclude = {SecurityAutoConfiguration.class})
+@SpringBootTest
 class EventManagementSystemApplicationTests {
 
     @Test
